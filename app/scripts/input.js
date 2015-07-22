f0f6491685802436bddd7177eee14c64f0301cf5
unknown <andy.yu@TCIT-RD7.tcit.local>
2015-07-22 11:28:56 +0800

    Watch minView and format attributes changes

--- app/scripts/input.js
+++ app/scripts/input.js
@@ -1,23 +1,23 @@
 'use strict';
 
 var PRISTINE_CLASS = 'ng-pristine',
-    DIRTY_CLASS = 'ng-dirty';
+  DIRTY_CLASS = 'ng-dirty';
 
 var Module = angular.module('datePicker');
 
 Module.constant('dateTimeConfig', {
-  template: function (attrs) {
+  template: function(attrs) {
     return '' +
-        '<div ' +
-        'date-picker="' + attrs.ngModel + '" ' +
-        (attrs.view ? 'view="' + attrs.view + '" ' : '') +
-        (attrs.maxView ? 'max-view="' + attrs.maxView + '" ' : '') +
-        (attrs.autoClose ? 'auto-close="' + attrs.autoClose + '" ' : '') +
-        (attrs.template ? 'template="' + attrs.template + '" ' : '') +
-        (attrs.minView ? 'min-view="' + attrs.minView + '" ' : '') +
-        (attrs.partial ? 'partial="' + attrs.partial + '" ' : '') +
-        (attrs.step ? 'step="' + attrs.step + '" ' : '') +
-        'class="date-picker-date-time"></div>';
+      '<div ' +
+      'date-picker="' + attrs.ngModel + '" ' +
+      (attrs.view ? 'view="' + attrs.view + '" ' : '') +
+      (attrs.maxView ? 'max-view="' + attrs.maxView + '" ' : '') +
+      (attrs.autoClose ? 'auto-close="' + attrs.autoClose + '" ' : '') +
+      (attrs.template ? 'template="' + attrs.template + '" ' : '') +
+      (attrs.minView ? 'min-view="' + attrs.minView + '" ' : '') +
+      (attrs.partial ? 'partial="' + attrs.partial + '" ' : '') +
+      (attrs.step ? 'step="' + attrs.step + '" ' : '') +
+      'class="date-picker-date-time"></div>';
   },
   format: 'yyyy-MM-dd HH:mm',
   views: ['date', 'year', 'month', 'hours', 'minutes'],
@@ -25,10 +25,10 @@ Module.constant('dateTimeConfig', {
   position: 'relative'
 });
 
-Module.directive('dateTimeAppend', function () {
+Module.directive('dateTimeAppend', function() {
   return {
-    link: function (scope, element) {
-      element.bind('click', function () {
+    link: function(scope, element) {
+      element.bind('click', function() {
         element.find('input')[0].focus();
       });
     }
@@ -36,137 +36,161 @@ Module.directive('dateTimeAppend', function () {
 });
 
 Module.directive('dateTime', ['$compile', '$document', '$filter', 'dateTimeConfig', '$parse', 'datePickerUtils',
-                function ($compile, $document, $filter, dateTimeConfig, $parse, datePickerUtils) {
-  var body = $document.find('body');
-  var dateFilter = $filter('date');
+  function($compile, $document, $filter, dateTimeConfig, $parse, datePickerUtils) {
+    var body = $document.find('body');
+    var dateFilter = $filter('date');
 
-  return {
-    require: 'ngModel',
-    scope:true,
-    link: function (scope, element, attrs, ngModel) {
-      var format = attrs.format || dateTimeConfig.format;
-      var parentForm = element.inheritedData('$formController');
-      var views = $parse(attrs.views)(scope) || dateTimeConfig.views.concat();
-      var view = attrs.view || views[0];
-      var index = views.indexOf(view);
-      var dismiss = attrs.autoClose ? $parse(attrs.autoClose)(scope) : dateTimeConfig.autoClose;
-      var picker = null;
-      var position = attrs.position || dateTimeConfig.position;
-      var container = null;
+    return {
+      require: 'ngModel',
+      scope: true,
+      link: function(scope, element, attrs, ngModel) {
+        var format = attrs.format || dateTimeConfig.format;
+        var parentForm = element.inheritedData('$formController');
+        var views = $parse(attrs.views)(scope) || dateTimeConfig.views.concat();
+        var view = attrs.view || views[0];
+        var index = views.indexOf(view);
+        var dismiss = attrs.autoClose ? $parse(attrs.autoClose)(scope) : dateTimeConfig.autoClose;
+        var picker = null;
+        var position = attrs.position || dateTimeConfig.position;
+        var container = null;
 
-      if (index === -1) {
-        views.splice(index, 1);
-      }
+        if (index === -1) {
+          views.splice(index, 1);
+        }
 
-      views.unshift(view);
-
-
-      function formatter(value) {
-        return dateFilter(value, format);
-      }
-
-      function parser() {
-        return ngModel.$modelValue;
-      }
-
-      ngModel.$formatters.push(formatter);
-      ngModel.$parsers.unshift(parser);
+        views.unshift(view);
 
 
-      //min. max date validators
-      if (angular.isDefined(attrs.minDate)) {
-        var minVal;
-        ngModel.$validators.min = function (value) {
+        function formatter(value) {
+          return dateFilter(value, format);
+        }
+
+        function parser() {
+          return ngModel.$modelValue;
+        }
+
+        ngModel.$formatters.push(formatter);
+        ngModel.$parsers.unshift(parser);
+
+
+        //min. max date validators
+        if (angular.isDefined(attrs.minDate)) {
+          var minVal;
+          ngModel.$validators.min = function(value) {
             return !datePickerUtils.isValidDate(value) || angular.isUndefined(minVal) || value >= minVal;
           };
-        attrs.$observe('minDate', function (val) {
+          attrs.$observe('minDate', function(val) {
             minVal = new Date(val);
             ngModel.$validate();
           });
-      }
+        }
 
-      if (angular.isDefined(attrs.maxDate)) {
-        var maxVal;
-        ngModel.$validators.max = function (value) {
+        if (angular.isDefined(attrs.maxDate)) {
+          var maxVal;
+          ngModel.$validators.max = function(value) {
             return !datePickerUtils.isValidDate(value) || angular.isUndefined(maxVal) || value <= maxVal;
           };
-        attrs.$observe('maxDate', function (val) {
+          attrs.$observe('maxDate', function(val) {
             maxVal = new Date(val);
             ngModel.$validate();
           });
-      }
-      //end min, max date validator
+        }
+        //end min, max date validator
 
-      var template = dateTimeConfig.template(attrs);
+        var template = dateTimeConfig.template(attrs);
 
-      function updateInput(event) {
-        event.stopPropagation();
-        if (ngModel.$pristine) {
-          ngModel.$dirty = true;
-          ngModel.$pristine = false;
-          element.removeClass(PRISTINE_CLASS).addClass(DIRTY_CLASS);
-          if (parentForm) {
-            parentForm.$setDirty();
+        // Watch attributes change
+        attrs.$observe('minView', function(value) {
+          if (value) {
+            template = dateTimeConfig.template(attrs);
           }
-          ngModel.$render();
-        }
-      }
-
-      function clear() {
-        if (picker) {
-          picker.remove();
-          picker = null;
-        }
-        if (container) {
-          container.remove();
-          container = null;
-        }
-      }
-
-      function showPicker() {
-        if (picker) {
-          return;
-        }
-        // create picker element
-        picker = $compile(template)(scope);
-        scope.$digest();
-
-        scope.$on('setDate', function (event, date, view) {
-          updateInput(event);
-          if (dismiss && views[views.length - 1] === view) {
-            clear();
+        });
+        attrs.$observe('format', function(value) {
+          if (value) {
+            format = value;
+            ngModel.$formatters.splice(-1, 1, formatter);
           }
         });
 
-        scope.$on('hidePicker', function () {
-          element.triggerHandler('blur');
-        });
-
-        scope.$on('$destroy', clear);
-
-        // move picker below input element
-
-        if (position === 'absolute') {
-          var pos = angular.extend(element.offset(), { height: element[0].offsetHeight });
-          picker.css({ top: pos.top + pos.height, left: pos.left, display: 'block', position: position});
-          body.append(picker);
-        } else {
-          // relative
-          container = angular.element('<div date-picker-wrapper></div>');
-          element[0].parentElement.insertBefore(container[0], element[0]);
-          container.append(picker);
-//          this approach doesn't work
-//          element.before(picker);
-          picker.css({top: element[0].offsetHeight + 'px', display: 'block'});
+        function updateInput(event) {
+          event.stopPropagation();
+          if (ngModel.$pristine) {
+            ngModel.$dirty = true;
+            ngModel.$pristine = false;
+            element.removeClass(PRISTINE_CLASS).addClass(DIRTY_CLASS);
+            if (parentForm) {
+              parentForm.$setDirty();
+            }
+            ngModel.$render();
+          }
         }
 
-        picker.bind('mousedown', function (evt) {
-          evt.preventDefault();
-        });
-      }
+        function clear() {
+          if (picker) {
+            picker.remove();
+            picker = null;
+          }
+          if (container) {
+            container.remove();
+            container = null;
+          }
+        }
 
-      element.bind('focus', showPicker);
-      element.bind('blur', clear);
-    }
-  };
-}]);
+        function showPicker() {
+          if (picker) {
+            return;
+          }
+          // create picker element
+          picker = $compile(template)(scope);
+          scope.$digest();
+
+          scope.$on('setDate', function(event, date, view) {
+            updateInput(event);
+            if (dismiss && views[views.length - 1] === view) {
+              clear();
+            }
+          });
+
+          scope.$on('hidePicker', function() {
+            element.triggerHandler('blur');
+          });
+
+          scope.$on('$destroy', clear);
+
+          // move picker below input element
+
+          if (position === 'absolute') {
+            var pos = angular.extend(element.offset(), {
+              height: element[0].offsetHeight
+            });
+            picker.css({
+              top: pos.top + pos.height,
+              left: pos.left,
+              display: 'block',
+              position: position
+            });
+            body.append(picker);
+          } else {
+            // relative
+            container = angular.element('<div date-picker-wrapper></div>');
+            element[0].parentElement.insertBefore(container[0], element[0]);
+            container.append(picker);
+            //          this approach doesn't work
+            //          element.before(picker);
+            picker.css({
+              top: element[0].offsetHeight + 'px',
+              display: 'block'
+            });
+          }
+
+          picker.bind('mousedown', function(evt) {
+            evt.preventDefault();
+          });
+        }
+
+        element.bind('focus', showPicker);
+        element.bind('blur', clear);
+      }
+    };
+  }
+]);
